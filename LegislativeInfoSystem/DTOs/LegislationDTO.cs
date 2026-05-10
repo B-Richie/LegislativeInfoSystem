875ee8045d604b20d9917ced6e3eb7f42cc3e47b
B-Rchie <rbakos37@gmail.com>
2026-05-10 11:41:39 -0400

    added regex for sanitizing; added css

--- LegislativeInfoSystem/DTOs/LegislationDTO.cs
+++ LegislativeInfoSystem/DTOs/LegislationDTO.cs
@@ -8,9 +8,11 @@ namespace LegislativeInfoSystem.DTOs
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Legislation Title is required.")]
+        [RegularExpression(@"^[^<>{}\[\]]+$", ErrorMessage = "Invalid characters are not allowed.")]
         public string? Title { get; set; }
 
         [Required(ErrorMessage = "Legislation Text is required.")]
+        [RegularExpression(@"^[^<>{}\[\]]+$", ErrorMessage = "Invalid characters are not allowed.")]
         public string? LegislationText { get; set; }
 
         public List<int> SponsorIds { get; set; } = [];
--- LegislativeInfoSystem/DTOs/LegislatorDTO.cs
+++ LegislativeInfoSystem/DTOs/LegislatorDTO.cs
@@ -6,12 +6,15 @@ namespace LegislativeInfoSystem.DTOs
     public class LegislatorDTO
     {
         [Required(ErrorMessage = "First Name is required.")]
+        [RegularExpression(@"^[^<>{}\[\]]+$", ErrorMessage = "Invalid characters are not allowed.")]
         public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required.")]
+        [RegularExpression(@"^[^<>{}\[\]]+$", ErrorMessage = "Invalid characters are not allowed.")]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Hometown is required.")]
+        [RegularExpression(@"^[^<>{}\[\]]+$", ErrorMessage = "Invalid characters are not allowed.")]
         public string? Hometown { get; set; }
 
     }
